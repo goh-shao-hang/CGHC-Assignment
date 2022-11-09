61a9e87eece28d004b2cc76daf72ead7fc4de846
FlintWKH <flint.wongkh@gmail.com>
2022-11-09 13:12:18 +0800

    OpeningUpdate

--- CGHC Assignment/Assets/_Scripts/MainMenu.cs
+++ CGHC Assignment/Assets/_Scripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
